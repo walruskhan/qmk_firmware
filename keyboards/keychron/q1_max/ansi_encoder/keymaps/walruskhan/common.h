4fd77950e40f592b774d846d50feb37981eb9386
greg <gregjc@protonmail..com>
2025-03-15 23:15:59 +1300

    Fn+S on mini mode prints \_(e)_/

---
+++ keyboards/keychron/q1_max/ansi_encoder/keymaps/walruskhan/common.h
@@ -0,0 +1,13 @@
+#include QMK_KEYBOARD_H
+
+enum layers {
+    WUMBO_BASE,
+    WUMBO_FN,
+    MINI_BASE,
+    MINI_FN,
+};
+
+enum custom_keycodes {
+    KC_SHRUG,
+
+};
---
+++ keyboards/keychron/q1_max/ansi_encoder/keymaps/walruskhan/rules.mk
@@ -0,0 +1,4 @@
+# This file intentionally left blank
+
+# https://docs.qmk.fm/features/send_string
+SEND_STRING_ENABLE    = yes
